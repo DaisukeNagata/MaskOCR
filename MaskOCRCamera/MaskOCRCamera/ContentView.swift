8b61cd08f78b5afe656c4b643339a05d896c7742
大祐永田 <dbank0208@gmail.com>
2021-07-29 20:05:15 +0900

    update: 1.12.4

--- MaskOCRCamera/MaskOCRCamera/ContentView.swift
+++ MaskOCRCamera/MaskOCRCamera/ContentView.swift
@@ -66,7 +66,6 @@ struct ContentView: View {
                              avFoundationVM: ocrMask,
                              maskViewDesign: maskViewDesign)
             }
-            .padding()
         }.onAppear {
             setting()
         }
